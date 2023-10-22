f5f44310b029170c86cdfa9b83578d1221442d08
Esteban Rebuffé-Mareau <estebanianis@gmail.com>
2023-10-22 08:10:29 +0200

    Init fullstack app

---
+++ back/src/main/java/configuration/SecurityConfig.java
@@ -0,0 +1,48 @@
+package configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+        private final JwtAuthenticationFilter jwtAuthenticationFilter;
+        private final AuthenticationProvider authProvider;
+        private static final String[] routesWhitesList = {
+                        "/api/auth/register",
+                        "/api/auth/login",
+        };
+
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                return http
+                                .csrf(csrf -> csrf
+                                                .disable())
+                                .authorizeHttpRequests(authRequest -> authRequest
+                                                .requestMatchers(routesWhitesList)
+                                                .permitAll()
+                                                .anyRequest().authenticated())
+                                .sessionManagement(sessionManager -> sessionManager
+                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .authenticationProvider(authProvider)
+                                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                                .build();
+
+        }
+
+        public void configure(WebSecurity web) throws Exception {
+                web.ignoring().requestMatchers("/swagger-ui/**", "/v3/api-docs/**");
+        }
+
+}
---
+++ ressources/sql/script.sql
@@ -0,0 +1,33 @@
+CREATE TABLE `USERS` (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    CONSTRAINT email_unique UNIQUE (email)
+);
+
+CREATE TABLE `SUBJECTS` (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `ARTICLES` (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    subject_id INT,
+    author_id INT,
+    publication_date DATETIME NOT NULL,
+    FOREIGN KEY (subject_id) REFERENCES Subjects(id),
+    FOREIGN KEY (author_id) REFERENCES Users(id)
+);
+
+CREATE TABLE `COMMENTS` (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    content TEXT NOT NULL,
+    article_id INT,
+    author_id INT,
+    publication_date DATETIME NOT NULL,
+    FOREIGN KEY (article_id) REFERENCES Articles(id),
+    FOREIGN KEY (author_id) REFERENCES Users(id)
+);
