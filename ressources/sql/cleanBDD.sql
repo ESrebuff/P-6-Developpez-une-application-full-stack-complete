9f69111b347cc2bc15ccbb824746e867e3783f0b
Esteban Rebuffé-Mareau <estebanianis@gmail.com>
2023-11-26 13:29:27 +0100

    add an reset for the sql tables

--- ressources/sql/cleanBDD.sql
+++ ressources/sql/cleanBDD.sql
@@ -1,17 +1,15 @@
 -- Delete all tables
-DROP TABLE COMMENTS;
-DROP TABLE ARTICLES;
-DROP TABLE SUBJECTS;
-DROP TABLE USERS;
+DROP DATABASE mdd;
+CREATE DATABASE mdd;
+USE mdd;
 
--- Create all tables
 CREATE TABLE `USERS` (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP,
-    updated_at TIMESTAMP,
+    updated_at TIMESTAMP
 );
 
 CREATE TABLE `SUBJECTS` (
@@ -21,13 +19,21 @@ CREATE TABLE `SUBJECTS` (
     updated_at TIMESTAMP
 );
 
+CREATE TABLE SUBSCRIPTIONS (
+    user_id INT,
+    subject_id INT,
+    subscription_date TIMESTAMP,
+    PRIMARY KEY (user_id, subject_id),
+    FOREIGN KEY (user_id) REFERENCES USERS(id),
+    FOREIGN KEY (subject_id) REFERENCES SUBJECTS(id)
+);
+
 CREATE TABLE `ARTICLES` (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     content TEXT NOT NULL,
     subject_id INT,
     author_id INT,
-    publication_date DATETIME NOT NULL,
     created_at TIMESTAMP,
     updated_at TIMESTAMP,
     FOREIGN KEY (subject_id) REFERENCES Subjects(id),
@@ -39,12 +45,13 @@ CREATE TABLE `COMMENTS` (
     content TEXT NOT NULL,
     article_id INT,
     author_id INT,
-    publication_date DATETIME NOT NULL,
     created_at TIMESTAMP,
     updated_at TIMESTAMP,
     FOREIGN KEY (article_id) REFERENCES Articles(id),
     FOREIGN KEY (author_id) REFERENCES Users(id)
 );
+
+
 
 INSERT INTO SUBJECTS (name, created_at, updated_at) VALUES
 ('Science', NOW(), NOW()),
