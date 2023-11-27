39f63b38cef1fbc2eb39c2f314441cd7bc99a346
Esteban Rebuffé-Mareau <estebanianis@gmail.com>
2023-11-27 15:29:15 +0100

    init pages

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,13 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ArticlesListComponent } from './pages/articles-list/articles-list.component';
+import { ArticleDetailsComponent } from './pages/article-details/article-details.component';
+import { ThemesListComponent } from './pages/themes-list/themes-list.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { CreateArticleComponent } from './pages/create-article/create-article.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+
 
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'articles', component: ArticlesListComponent },
+  { path: 'article/:id', component: ArticleDetailsComponent },
+  { path: 'themes', component: ThemesListComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'create-article', component: CreateArticleComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,9 +5,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ArticlesListComponent } from './pages/articles-list/articles-list.component';
+import { ArticleDetailsComponent } from './pages/article-details/article-details.component';
+import { ThemesListComponent } from './pages/themes-list/themes-list.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { CreateArticleComponent } from './pages/create-article/create-article.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, ArticlesListComponent, ArticleDetailsComponent, ThemesListComponent, ProfileComponent, CreateArticleComponent, LoginComponent, RegisterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
